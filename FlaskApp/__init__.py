0f96977d0a859e77a24431d89ad4ce100ac0f9b1
Lartg <roc.gerrety@gmail.com>
2022-03-18 11:08:22 -0700

    fixed sqlalchemy being really stupid for reducing compatibility

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -15,7 +15,7 @@ migrate = Migrate(compare_type=True)
 
 db = SQLAlchemy()
 
-uri = os.environ.get('DATABASE_URL')
+uri = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
